b1179866764f57eee363afa29f4bc08946f4d7b7
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2024-01-29 00:17:18 -0700

    Project Part 3 / Guessing game started.

---
+++ GuessingGame.java
@@ -0,0 +1,7 @@
+public class GuessingGame {
+
+    public GuessingGame() {
+        System.out.println("Greetings Earthling!");
+    }
+    
+}
---
+++ Mars.java
@@ -0,0 +1,39 @@
+public class Mars {
+    public static void main(String[] args) throws InterruptedException {
+        String colonyName = "Liberty Station";
+        int shipPopulation = 300;
+        double meals = 4000.00;
+        boolean landing = true;
+
+        meals = meals - shipPopulation * .75;
+        meals = meals - shipPopulation * .75;
+        System.out.println(meals);
+        meals = meals + (meals * .5);
+        shipPopulation = shipPopulation + 5;
+
+        String landingLocation = "The Ocean";
+        if (landingLocation.equalsIgnoreCase("The Plain")) {
+            System.out.println("Bbzzz Landing on the Plain");
+        } else {
+            System.out.println("ERROR!!! FLight plan already set. Landing on the Plain");
+        }
+        landing = landingCheck(25);
+    }
+
+    public static boolean landingCheck(int minutesLeft) throws InterruptedException {
+        for (int minute = 0; minute <= minutesLeft; minute++) {
+            if (((minute % 3) == 0) && ((minute % 2) == 0)) {
+                System.out.println("Keep Center");
+            } else if ((minute % 2) == 0) {
+                System.out.println("Right");
+            } else if ((minute % 3) == 0) {
+                System.out.println("Left");
+            } else {
+                System.out.println("Calculating");
+            }
+            Thread.sleep(250);
+        }
+        System.out.println("Landed");
+        return false;
+    }
+}
