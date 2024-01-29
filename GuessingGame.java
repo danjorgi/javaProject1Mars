2bfc81be5a0e47d3f93ea950d9714954efbd8c64
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2024-01-29 01:04:21 -0700

    Added Random number generator, and if/else for guessing number.

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessingGame {
@@ -9,6 +10,29 @@ public class GuessingGame {
         Scanner input = new Scanner(System.in);
 
         String name = input.nextLine();
+
+        System.out.println("Alright " + name + ", I'm thinking of a number between 1 and 100.");
+        System.out.println("Can you guess what it is? (Type a number.)");
+
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+        int tries = 0;
+
+        while (true) {
+            int guess = input.nextInt();
+            
+            tries = tries + 1;
+
+            if (guess < number) {
+                System.out.println("Your guess is too low, try again.");
+            } else if (guess > number) {
+                System.out.println("Your guess is too high, try again.");
+            } else {
+                System.out.println("Well done " + name);
+                System.out.println("You found my number in " + tries + " tries!");
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
